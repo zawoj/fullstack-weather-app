d747e2da30507070bfa10b2e7551eed8f76ebebc
Zawoj <zawojskikacperkontakt@gmail.com>
2024-01-09 18:03:08 +0100

    feat(exception): create custom filter exception

--- gmi-back-weather/src/app.module.ts
+++ gmi-back-weather/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { WeathersModule } from './weathers/weathers.module';
 
 @Module({
-  imports: [],
+  imports: [WeathersModule],
   controllers: [AppController],
   providers: [AppService],
 })
