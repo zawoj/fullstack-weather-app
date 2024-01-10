f7ba88ef98c150a49963b6cb8f8200e259b9dc77
Zawoj <zawojskikacperkontakt@gmail.com>
2024-01-10 13:59:32 +0100

    fix: HTTPs service not imported

--- gmi-back-weather/src/weathers/weathers.module.ts
+++ gmi-back-weather/src/weathers/weathers.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { WeathersService } from './weathers.service';
 import { WeathersController } from './weathers.controller';
 import { CacheModule } from '@nestjs/cache-manager';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
   imports: [
     CacheModule.register({
       ttl: 90,
     }),
+    HttpModule,
   ],
   controllers: [WeathersController],
   providers: [WeathersService],
